eeb799fe6ae3240b524ecafccfc5b9bdd9c765e8
Hachi <swordarchor@gmail.com>
2015-11-01 12:01:30 +0800

    update, support multiple folders

--- PhantomCSS/libs/visr.js
+++ PhantomCSS/libs/visr.js
@@ -6,221 +6,222 @@
 var fs = require('fs');
 var phantomcss = require('../phantomcss.js');
 
-// consts - selector
+// consts - dom selector
 var login_component = {
-  container: '#lb_LoginStyle',
-  user: '#Uid',
-  pwd: '#Pwd',
-  submit: '.mstrLoginButtonBarLeft input'
-};
-
-var folder_container = '#folderAllModes';
-var dashboard_links = '#folderAllModes span[sty="nm"] a';
-var viz_container = '.mstrmojo-VIDocLayout .mstrmojo-VIPanel-content .mstrmojo-VIVizPanel-content';
-var panelseletor = '.mstrmojo-DockedPanelSelector .panel-list .item';
-var panelcontainer = '.mstrmojo-VIVizPanel-content .mstrmojo-VIDocRelativePanel:nth-child';
-var mstrWaitBox = "#waitBox .mstrWaitBox";
-
-
-var VI_links = [];
+    container: '#lb_LoginStyle',
+    user: '#Uid',
+    pwd: '#Pwd',
+    submit: '.mstrLoginButtonBarLeft input'
+    },
+    folder_container = '#folderAllModes',
+    dashboard_links = '#folderAllModes span[sty="nm"] a',
+    viz_container = '.mstrmojo-VIDocLayout .mstrmojo-VIPanel-content .mstrmojo-VIVizPanel-content',
+    panelseletor = '.mstrmojo-DockedPanelSelector .panel-list .item',
+    panelcontainer = '.mstrmojo-VIVizPanel-content .mstrmojo-VIDocRelativePanel:nth-child',
+    mstrWaitBox = "#waitBox .mstrWaitBox",
+    VI_links = [],
+    vizcount = 0;
 
 
 exports.staticRendering = staticRendering;
 
 function staticRendering(config_info, folder_list) {
-  for (var i = 0; i < folder_list.length; i++) {
-    _srSingleFolder(config_info, folder_list[i]);
-  }
+    casper.test.begin('Mojo visual tests - ' + config_info.testsuite, function(test) {
+        var outputRoot = '/' + config_info.testsuite,
+            comparisonResultRoot = fs.workingDirectory + '/output' + outputRoot,
+            failedComparisonsRoot = fs.workingDirectory + '/failures' + outputRoot,
+            i, il;
+
+        phantomcss.init({
+            rebase: casper.cli.get("rebase"),
+            casper: casper,
+            libraryRoot: fs.absolute(fs.workingDirectory + ''),
+            screenshotRoot: fs.absolute(fs.workingDirectory + '/baselines' + outputRoot),
+            comparisonResultRoot: fs.absolute(comparisonResultRoot),
+            failedComparisonsRoot: fs.absolute(failedComparisonsRoot),
+            addLabelToFailedImage: false,
+        });
+
+        casper.on('remote.message', function(msg) {
+            this.echo(msg);
+        })
+
+        casper.on('error', function(err) {
+            this.die("PhantomJS has errored: " + err);
+        });
+
+        casper.on('resource.error', function(err) {
+            casper.log('Resource load error: ' + err, 'warning');
+        });
+
+        // configuration for casper
+        casper.options.waitTimeout = config_info.waitTimeout;
+        casper.options.viewportSize = {
+            width: config_info.viewsize.width,
+            height: config_info.viewsize.height
+        };
+
+        casper.start(config_info.login_url);
+
+        // delete existing output and failures folder
+        casper.then(function() {
+            if (fs.exists(comparisonResultRoot)) {
+                console.log("Deleting related output folder...");
+                fs.removeTree(comparisonResultRoot);
+            }
+            if (fs.exists(failedComparisonsRoot)) {
+                console.log("Deleting related failures folder...");
+                fs.removeTree(failedComparisonsRoot);
+            }
+        })
+
+        casper.then(function() {
+            // wait for login container to appear 
+            casper.waitForSelector(login_component.container,
+                function success() {
+                    casper.evaluate(function(username, password, login_component) {
+                        document.querySelector(login_component.user).value = username;
+                        document.querySelector(login_component.pwd).value = password;
+                        document.querySelector(login_component.submit).click();
+                    }, config_info.username, config_info.password, login_component);
+                    console.log("Login...");
+                },
+                function timeout() {
+                    casper.test.fail('Should see Login container');
+                }
+            )
+        });
+
+        console.log("Start to open test folder one by one...");
+        casper.eachThen(folder_list, function(folderItem) {
+            _srSingleFolder(config_info, folderItem.data);
+        });
+
+        casper.then(function now_check_the_screenshots() {
+            phantomcss.compareAll();
+        });
+
+        /*
+        Casper runs tests
+        */
+        casper.run(function() {
+            console.log('\nTHE END.');
+            // phantomcss.getExitStatus() // pass or fail?
+            casper.test.done();
+        });
+    });
 }
 
 
 function _srSingleFolder(config_info, folder) {
-  casper.test.begin('Mojo visual tests - ' + config_info.testsuite, function(test) {
-    var outputRoot = /*'/' + config_info.testsuite + */'/' + folder.name;
-    var comparisonResultRoot = fs.workingDirectory + '/output' + outputRoot;
-    var failedComparisonsRoot = fs.workingDirectory + '/failures' + outputRoot;
-    phantomcss.init({
-      rebase: casper.cli.get("rebase"),
-      casper: casper,
-      libraryRoot: fs.absolute(fs.workingDirectory + ''),
-      screenshotRoot: fs.absolute(fs.workingDirectory + '/baselines' + outputRoot),
-      comparisonResultRoot: fs.absolute(comparisonResultRoot),
-      failedComparisonsRoot: fs.absolute(failedComparisonsRoot),
-      addLabelToFailedImage: false,
-    });
-
-    casper.on('remote.message', function(msg) {
-      this.echo(msg);
-    })
-
-    casper.on('error', function(err) {
-      this.die("PhantomJS has errored: " + err);
-    });
-
-    casper.on('resource.error', function(err) {
-      casper.log('Resource load error: ' + err, 'warning');
-    });
-
-    // configuration for casper
-    casper.options.waitTimeout = config_info.waitTimeout;
-    casper.options.viewportSize = {
-      width: config_info.viewsize.width,
-      height: config_info.viewsize.height
-    };
-
-    casper.start(config_info.login_url);
-
-    // delete existing output and failures folder
-    casper.then(function() {
-      if (fs.exists(comparisonResultRoot)) {
-        console.log("Deleting related output folder...");
-        fs.removeTree(comparisonResultRoot);
-      }
-      if (fs.exists(failedComparisonsRoot)) {
-        console.log("Deleting related failures folder...");
-        fs.removeTree(failedComparisonsRoot);
-      }
-    })
-
-    casper.then(function() {
-      // wait for login container to appear 
-      casper.waitForSelector(login_component.container,
-        function success() {
-          casper.evaluate(function(username, password, login_component) {
-            document.querySelector(login_component.user).value = username;
-            document.querySelector(login_component.pwd).value = password;
-            document.querySelector(login_component.submit).click();
-          }, config_info.username, config_info.password, login_component);
-          console.log("Login...");
-        },
-        function timeout() {
-          casper.test.fail('Should see Login container');
-        }
-      )
-    });
-
     // get VI lists in one folder
     casper.thenOpen(folder.url, function() {
-      casper.waitForSelector(folder_container,
-        function success() {
-          console.log("Navigate to target folder successfully...");
-        },
-        function timeout() {
-          casper.test.fail('Should see Folder lists');
-        }
-      );
+        casper.waitForSelector(folder_container,
+            function success() {
+                console.log(folder.name + ": Navigate to this target folder successfully...");
+            },
+            function timeout() {
+                casper.test.fail(folder.name + ": Fail to open this target folder");
+            }
+        );
 
-      VI_links = casper.evaluate(function(folder_container, dashboard_links) {
-        var links = document.querySelectorAll(dashboard_links);
-        var count = links.length;
-        var lists = [];
-        for (var i = 0; i < count; i++) {
-          lists.push(links[i].href);
-        }
-        return lists;
-      }, folder_container, dashboard_links);
+        VI_links = casper.evaluate(function(folder_container, dashboard_links) {
+            var links = document.querySelectorAll(dashboard_links),
+                count = links.length,
+                lists = [];
+            for (var i = 0; i < count; i++) {
+                lists.push(links[i].href);
+            }
+            return lists;
+        }, folder_container, dashboard_links);
     });
 
     // run dashboards one by one
     casper.then(function() {
-      console.log("Start to run dashboard one by one...");
-      casper.eachThen(VI_links, function(viz_link) {
-        _runDashboard(viz_link.data);
-      });
+        console.log(folder.name + ": Start to run dashboard one by one...");
+        casper.eachThen(VI_links, function(viz_link) {
+            _runDashboard(viz_link.data, folder.name, folder.waitTime);
+        });
     });
-
-    casper.then(function now_check_the_screenshots() {
-      phantomcss.compareAll();
-    });
-
-    /*
-    Casper runs tests
-    */
-    casper.run(function() {
-      console.log('\nTHE END.');
-      // phantomcss.getExitStatus() // pass or fail?
-      casper.test.done();
-    });
-  });
 }
 
 
-var vizcount = 0;
+function _runDashboard(viz_url, folderName, waitTime) {
+    var data = _parseURL(viz_url);
 
-function _runDashboard(viz_url) {
-  var data = _parseURL(viz_url);
-
-  casper.thenOpen(config_info.webserver, {
-      method: "post",
-      data: data
+    casper.thenOpen(config_info.webserver, {
+        method: "post",
+        data: data
     }, function() {
         this.echo("Dashboard request has been sent successfully.")
     });
 
-  casper.waitUntilVisible(viz_container,
-    function success() {
-      var viz_name = (casper.getTitle().split("."))[0];
-
-      casper.waitWhileVisible(mstrWaitBox,
+    casper.waitUntilVisible(viz_container,
         function success() {
-          vizcount++;
+            var viz_name = (casper.getTitle().split("."))[0];
 
-          console.log(viz_name);
-
-          casper.then(function() {
-            // get the count of panels
-            var count = casper.evaluate(function() {
-              return (document.querySelectorAll('.mstrmojo-DockedPanelSelector .panel-list .item')).length;
-            });
-
-            var j = 0;
-            casper.repeat(count, function() {
-              var idx = j + 1;
-
-              casper.mouseEvent('click', panelseletor + '[idx="' + j + '"]');
-
-              casper.waitUntilVisible(panelcontainer + '(' + idx + ')',
+            casper.waitWhileVisible(mstrWaitBox,
                 function success() {
+                    vizcount++;
 
-                  casper.waitWhileVisible(mstrWaitBox,
-                    function success() {
-                      casper.wait(config_info.waitTime);
-                      console.log("panel_" + idx);
-                      phantomcss.screenshot(panelcontainer + '(' + idx + ')', viz_name + "_panel_" + idx);
-                      j++;
-                    },
-                    function timeout() {
-                      phantomcss.screenshot('html', viz_name + "_panel_" + idx + "loading_timeout");
+                    console.log(viz_name);
+
+                    casper.then(function() {
+                        // get the count of panels
+                        var count = casper.evaluate(function() {
+                            return (document.querySelectorAll('.mstrmojo-DockedPanelSelector .panel-list .item')).length;
+                        });
+
+                        var j = 0;
+                        casper.repeat(count, function() {
+                            var idx = j + 1;
+
+                            casper.mouseEvent('click', panelseletor + '[idx="' + j + '"]');
+
+                            casper.waitUntilVisible(panelcontainer + '(' + idx + ')',
+                                function success() {
+
+                                    casper.waitWhileVisible(mstrWaitBox,
+                                        function success() {
+                                            if (waitTime) {
+                                                casper.wait(waitTime);
+                                            }
+                                            console.log("panel_" + idx);
+                                            phantomcss.screenshot(panelcontainer + '(' + idx + ')', folderName + "/" + viz_name + "_panel_" + idx);
+                                            j++;
+                                        },
+                                        function timeout() {
+                                            phantomcss.screenshot('html', viz_name + "_panel_" + idx + "loading_timeout");
+                                        });
+                                },
+                                function timeout() {
+                                    phantomcss.screenshot('html', viz_name + "_panel_" + idx + "_timeout");
+                                    casper.thenBypass(count - j - 1);
+                                });
+                        });
                     });
                 },
                 function timeout() {
-                  phantomcss.screenshot('html', viz_name + "_panel_" + idx + "_timeout");
-                  casper.thenBypass(count - j - 1);
+                    phantomcss.screenshot('html', viz_name + "_timeout");
+                    //casper.test.fail('Should see Viz_' + viz_name + " loading End");
                 });
-            });
-          });
         },
         function timeout() {
-          phantomcss.screenshot('html', viz_name + "_timeout");
-          //casper.test.fail('Should see Viz_' + viz_name + " loading End");
-        });
-    },
-    function timeout() {
-      phantomcss.screenshot('html', vizcount + "_timeout");
-      //casper.test.fail('Should see Viz opened');
-    }
-  );
+            phantomcss.screenshot('html', viz_name + "_timeout");
+            //casper.test.fail('Should see Viz opened');
+        }
+    );
 }
 
 function _parseURL(viz_url) {
-  var data = {};
-  var paras = viz_url.split("?")[1].split("&");  
-  var para = "";
+    var data = {};
+    var paras = viz_url.split("?")[1].split("&");
+    var para = "";
 
-  for(var i = 0, len = paras.length; i < len; i++) {
-    para = paras[i].split("=");
-    data[para[0]] = para[1];
-  }
+    for (var i = 0, len = paras.length; i < len; i++) {
+        para = paras[i].split("=");
+        data[para[0]] = para[1];
+    }
 
-  return data;
+    return data;
 }
--- PhantomCSS/tests/userConfig.js
+++ PhantomCSS/tests/userConfig.js
@@ -4,35 +4,43 @@
 */
 
 var config_info = {
-  iserver: "VMHTML5VI",
-  port: "34952",
-  webserver: "http://localhost:8080/MicroStrategy/servlet/mstrWeb",
-  project_name: "MicroStrategy Tutorial",
-  username: "Automation",
-  password: "",
-  testsuite: 'VI_Automation',
-  viewsize: {
-    width: 1432,
-    height: 790
-  },
-  waitTimeout: 60000,
-  //waitTime: 50 // must > 0
+    iserver: "VMHTML5VI",
+    port: "34952",
+    webserver: "http://localhost:8080/MicroStrategy/servlet/mstrWeb",
+    project_name: "MicroStrategy Tutorial",
+    username: "administrator",
+    password: "",
+    testsuite: 'VI_Automation', // output folder name under "output"
+    viewsize: {
+        width: 1432,
+        height: 790
+    },
+    waitTimeout: 60000 // if response time > 60000, kill process
 };
 
-var folder_list = [{
-  name: 'VI_Acceptance', // output folder name
-  id: 'F11CAB7C4B887D887CB5D387D350AD65' // folder id
-}];
+// config for each folder
+var folder_list = [
+    {
+      name: 'VI_Acceptance', // output folder name under testsuite folder
+      id: 'F11CAB7C4B887D887CB5D387D350AD65' // folder id
+    },
+    {
+      name: 'VIZ_PreCheckin',
+      id: '2F3C710F460CA8956860129BFC0000C2',
+      waitTime: 60000 // must > 0, if you needn't add extra waiting time after waitbox disappears, just remove this prop
+    }
+];
 
-var fs = require('fs');
-var mstrWeb = require('../libs/mstrWeb.js');
-var visr = require('../libs/visr.js');
+var fs = require('fs'),
+    mstrWeb = require('../libs/mstrWeb.js'),
+    visr = require('../libs/visr.js'),
+    i, il;
 
 config_info.project_name = config_info.project_name.replace(" ", "+");
 config_info.login_url = mstrWeb.generateLoginURL(config_info);
 
-for (var i = 0; i < folder_list.length; i++) {
-  folder_list[i].url = mstrWeb.generateFolderURL(config_info, folder_list[i].id);
+for (i = 0, il = folder_list.length; i < il; i++) {
+    folder_list[i].url = mstrWeb.generateFolderURL(config_info, folder_list[i].id);
 }
 
 visr.staticRendering(config_info, folder_list);
